ebcf41a0f28801026508082424bfaa216700f254
Yuliia <umousee@gmail.com>
2022-11-22 16:33:28 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className='btn btn-primary'>
+          Hello
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
