de7cde31caeef92b0fb0ae33dd595b3f4090044e
Yuliia <umousee@gmail.com>
2022-12-05 18:28:06 +0100

    Fix spacing

--- src/Meaning.js
+++ src/Meaning.js
@@ -12,7 +12,7 @@ export default function Meaning(props){
                 return(
                     <div key={index}>
                         <p>
-                            <strong>Definition:</strong> 
+                            <strong>Definition: </strong> 
                             {definition.definition}
                             <br />
                             <em>{definition.example}</em>
